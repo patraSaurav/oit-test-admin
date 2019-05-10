866981da53c52caa07bc1b6427ed0101c406e81a
ThugHacker <33261277+ThugHacker@users.noreply.github.com>
2019-05-10 19:33:15 +0530

    router connection
    connect each other of hr,user and reviewer from dashboard using router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserDashboardComponent } from './dashboard/user-dashboard/user-dashboard.component';
+import { HrDashboardComponent } from './dashboard/hr-dashboard/hr-dashboard.component';
+import { ReviewerDashboardComponent } from './dashboard/reviewer-dashboard/reviewer-dashboard.component';
 
 const routes: Routes = [
-  { path: 'crisis-center' },
+  { path: '', component: DashboardComponent},
+  { path: 'user-dashboard', component: UserDashboardComponent },
+  {path: 'hr-dashboard', component: HrDashboardComponent },
+  {path: 'reviewer-dashboard', component: ReviewerDashboardComponent }
 ];
 
 @NgModule({
