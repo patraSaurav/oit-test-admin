e097b8e2b7fed81b3533d6b8db67e99246f15daf
ThugHacker <33261277+ThugHacker@users.noreply.github.com>
2019-05-13 19:45:50 +0530

    admin dashboard almost complete
    remove home page, added status and action table header in all user,hr and reviewer page also changes action icons.
    added popup modal view for action button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDashboardComponent } from './dashboard/user-dashboard/user-dashboard.component';
 import { HrDashboardComponent } from './dashboard/hr-dashboard/hr-dashboard.component';
 import { ReviewerDashboardComponent } from './dashboard/reviewer-dashboard/reviewer-dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent},
+  // { path: 'admin-dashboard', component: DashboardComponent },
+  { path: '',   redirectTo: '/user-dashboard', pathMatch: 'full' },
   { path: 'user-dashboard', component: UserDashboardComponent },
-  {path: 'hr-dashboard', component: HrDashboardComponent },
-  {path: 'reviewer-dashboard', component: ReviewerDashboardComponent }
+  { path: 'hr-dashboard', component: HrDashboardComponent },
+  { path: 'reviewer-dashboard', component: ReviewerDashboardComponent }
 ];
 
 @NgModule({
