f1defe476b43b6fea39f2ba31b146932fff923ef
ThugHacker <33261277+ThugHacker@users.noreply.github.com>
2019-05-14 18:58:53 +0530

    admin dashboard
    remove all html of hr and reviewer...now only use user html. added functionality to add person button but it is not completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { ReviewerDashboardComponent } from './dashboard/reviewer-dashboard/revie
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/user-dashboard/user-dashboard.component.ts
+++ src/app/dashboard/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
+ storePerson = new Array();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onStorePerson(form: NgForm) {
+
+    const firstname = form.value.firstname;
+    const lastname = form.value.lastname;
+    const email = form.value.email;
+    this.storePerson = [firstname, lastname, email];
+    const arr: Array<String> = this.storePerson;
+    console.log(arr);
+  }
+
+  // onStorePerson() {
+  //   this.storePerson = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  //   console.log(this.storePerson);
+  // }
+  // onAddPerson() {
+  //   console.log(this.storePerson);
+  // }
 
 }
