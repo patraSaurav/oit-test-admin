fc9fd735a8030bb37da7201d155870053092493a
ThugHacker <33261277+ThugHacker@users.noreply.github.com>
2019-05-17 19:38:25 +0530

    reviewer is incomplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { HrComponent } from './hr/hr.component';
+import { ReviewerComponent } from './reviewer/reviewer.component';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/admin', pathMatch: 'full' },
   { path: 'admin', component: AdminComponent },
   { path: 'hr', component: HrComponent },
-  // { path: 'reviewer-dashboard', component: ReviewerDashboardComponent }
+  { path: 'reviewer', component: ReviewerComponent }
 ];
 
 @NgModule({
